11c351bb6e22778d90fa3153719803fb32d76de7
Camen <camenpihor@gmail.com>
2020-03-31 14:53:59 -0700

    add logging

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,26 +1,31 @@
 """App tools and routing for the backend."""
+import logging
+
 from flask import Flask
 
 from .coviz import read_confirmed_time_series
 
 app = Flask(__name__)  # pylint: disable=invalid-name
+logging.basicConfig(level=logging.INFO)
+_logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
 @app.route("/")
 def health_check():
     """Ping test."""
+    _logger.info("Ping! health check...")
     return "Success"
 
 
 @app.route("/covid/confirmed/new/<date>")
 def get_new_confirmed(date):
     """Get new confirmed COVID-19 cases on date."""
-    print(date)
+    _logger.info("Getting new confirmed COVID-19 cases for %s", date)
     return read_confirmed_time_series().to_json()
 
 
 @app.route("/covid/confirmed/total/<date>")
 def get_confirmed_to_date(date):
     """Get total number of COVID-19 cases to date."""
-    print(date)
+    _logger.info("Getting total confirmed COVID-19 cases by %s", date)
     return read_confirmed_time_series().to_json()
