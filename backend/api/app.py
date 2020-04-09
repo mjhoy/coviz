9cf1fe0827c86aa14c7cfc43190507d557adec2b
Camen Piho <29082904+camenpihor@users.noreply.github.com>
2020-04-09 11:27:02 -0700

    Update tests; get specific date(s) (#11)

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,7 +1,9 @@
 """App tools and routing for the backend."""
+import datetime
 import logging
 
 from flask import Flask
+import pandas as pd
 
 from .coviz import read_confirmed_time_series
 
@@ -19,13 +21,26 @@ def health_check():
 
 @app.route("/covid/confirmed/new/<date>")
 def get_new_confirmed(date):
-    """Get new confirmed COVID-19 cases on date."""
-    _logger.info("Getting new confirmed COVID-19 cases for %s", date)
-    return read_confirmed_time_series().to_json()
+    """Get new confirmed COVID-19 cases to date."""
+    _logger.info("Getting new confirmed COVID-19 cases for %s")
+
+    date = datetime.datetime.strptime(date, "%Y-%m-%d")
+    time_series = read_confirmed_time_series()
+
+    data = time_series[date.strftime("%-m/%-d/%-y")].groupby(level="Country/Region").sum()
+    return data.to_json()
 
 
 @app.route("/covid/confirmed/total/<date>")
 def get_confirmed_to_date(date):
     """Get total number of COVID-19 cases to date."""
     _logger.info("Getting total confirmed COVID-19 cases by %s", date)
-    return read_confirmed_time_series().to_json()
+    start_date = datetime.datetime(2020, 1, 22)
+    end_date = datetime.datetime.strptime(date, "%Y-%m-%d")
+
+    time_series = read_confirmed_time_series().drop(columns=["Lat", "Long"])
+    date_range = pd.date_range(start_date, end_date).strftime("%-m/%-d/%-y")
+    date_range = set(date_range).intersection(set(time_series.columns))
+
+    data = time_series[date_range].groupby(level="Country/Region").sum().sum(axis=1)
+    return data.to_json()
--- backend/tests/test_coviz.py
+++ backend/tests/test_coviz.py
@@ -5,11 +5,10 @@ from api import coviz
 
 
 def test_read_confirmed_time_series():
-    non_date_columns = ["Province/State", "Country/Region", "Lat", "Long"]
+    index = ["Province/State", "Country/Region"]
+    non_date_columns = ["Lat", "Long"]
     date_columns = list(
-        pd.date_range(
-            start="2020-01-22", end="2020-04-03"
-        ).strftime("%-m/%-d/%-y")
+        pd.date_range(start="2020-01-22", end="2020-04-03").strftime("%-m/%-d/%-y")
     )
     columns = non_date_columns + date_columns
 
@@ -19,3 +18,4 @@ def test_read_confirmed_time_series():
     assert (
         set(columns) - set(actual.columns) == set()
     ), "Try running `git submodule update` and then re-running tests"
+    assert set(index) - set(actual.index.names) == set()
