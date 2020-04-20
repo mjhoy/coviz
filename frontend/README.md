01a68560479d5b2868717c027c264f3bf2f6dee6
Michael Hoy <mjh@mjhoy.com>
2020-04-20 08:52:18 -0400

    Initial elm dev; allow user to input date for total US cases (#12)
    * elm "hello world" dev server
    
    * initial parsing of the covid data for a specific date
    
    * allow user to input a date
    
    * fix strings
    
    * fix readme instructions
    
    * remove unused import
    
    * add a run-dev-server frontend script
    
    * remove public
    
    * just expose main
    
    * use module namespaces for html functions

---
+++ frontend/README.md
@@ -0,0 +1,13 @@
+# CoViz Frontend
+
+An elm app that displays COVID-19 data, using the backend api.
+
+## Installation
+
+1. Install `elm`: https://guide.elm-lang.org/install/ (0.19)
+
+## Development
+
+1. run `./scripts/run-dev-server` to start a frontend development server.
+2. Start up the backend server.
+3. You should now be able to load the site at http://localhost:8000/src/Main.elm
--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,22 +1,103 @@
-module Main exposing (..)
+module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, h1, text)
+import Html as H
+import Html.Attributes as HA
+import Html.Events as HE
+import Http
+import Json.Decode exposing (Decoder, field, int)
+
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
-init : ()
-init = ()
+
+type Model
+    = Waiting String
+    | Failure
+    | Loading String
+    | Success Int
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Waiting "", Cmd.none )
+
 
 type Msg
-  = Noop
+    = GotData (Result Http.Error Int)
+    | UpdateDateInput String
+    | Submit String
 
-update : Msg -> () -> ()
-update _ _ = ()
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Submit date ->
+            ( Loading date, getCovidData ("http://localhost:5000/covid/confirmed/total/" ++ date) )
+
+        GotData result ->
+            case result of
+                Ok data ->
+                    ( Success data, Cmd.none )
+
+                Err _ ->
+                    ( Failure, Cmd.none )
+
+        UpdateDateInput string ->
+            ( Waiting string, Cmd.none )
 
 
-view : () -> Html Msg
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> H.Html Msg
 view model =
-  div []
-    [ h1 [] [ text "Hello world!" ] ]
+    let
+        buildInputForm currentValue =
+            [ H.input
+                [ HA.type_ "text"
+                , HA.placeholder "date"
+                , HA.value currentValue
+                , HE.onInput UpdateDateInput
+                ]
+                []
+            , H.button [ HE.onClick (Submit currentValue) ] [ H.text "Submit" ]
+            ]
+
+        ( headerText, inputForm ) =
+            case model of
+                Failure ->
+                    ( "Something went wrong", buildInputForm "" )
+
+                Success data ->
+                    ( "Got some data! " ++ String.fromInt data, buildInputForm "" )
+
+                Loading url ->
+                    ( "Loading " ++ url, [] )
+
+                Waiting url ->
+                    ( "Enter a date (YYYY-MM-DD) for total US confirmed cases:", buildInputForm url )
+    in
+    H.div []
+        [ H.h1 [] [ H.text headerText ], H.div [] inputForm ]
+
+
+getCovidData : String -> Cmd Msg
+getCovidData url =
+    Http.get
+        { url = url
+        , expect = Http.expectJson GotData covidDecoder
+        }
+
+
+covidDecoder : Decoder Int
+covidDecoder =
+    field "US" int
